ae8fc987b2ae2ec237afe5b5dceee2e4d2c8d066
May Yeung <lightknightdark@gmail.com>
2023-08-01 01:14:31 +0800

    "activity_find_doctor xml"

--- app/src/main/java/com/example/healthcareapp/HomeActivity.java
+++ app/src/main/java/com/example/healthcareapp/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.example.healthcareapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.cardview.widget.CardView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("share_prefs", Context.MODE_PRIVATE);
+        String username  =  sharedPreferences.getString("username","").toString();
+
+        Toast.makeText(getApplicationContext(), "Login success", Toast.LENGTH_SHORT).show();
+
+
+        CardView exit = findViewById(R.id.cardExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivities(new Intent[]{new Intent(HomeActivity.this, LoginActivity.class)});
+
+            }
+        });
+
+        CardView findDoctor = findViewById(R.id.cardFindDoctor);
+        findDoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivities(new Intent[]{new Intent(HomeActivity.this, FindDoctorActivity.class)});
+            }
+        });
+
+
+
     }
 }
